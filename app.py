85c35293ef8f3739980f2956358a24d32768680c
yuminpa33 <myrte.yuminpa.ruban07@gmail.com>
2025-06-25 23:41:52 +0900

    import文修正

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.chat_models import AzureChatOpenAI
+from langchain_community.chat_models import AzureChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 from azure.cosmos import CosmosClient, PartitionKey
 import speech_recognition as sr
